5a04a87a3f4b2d36fd73c0d43a297a0e27eebcec
Ben Kane's Win Tower <bbk1524@gmail.com>
2015-09-27 00:47:06 -0500

    Static Libraries should be good now...

--- src/Source.cpp
+++ src/Source.cpp
@@ -10,7 +10,6 @@
 
 int main (int argc, char** argv)
 {
-
 	//init libraries at the top level to avoid game system dependencies and because I'm using SDL_main anyway
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 	IMG_Init(IMG_INIT_PNG);
