5a3193c4f08e7a18b9d4604f4b32a169e491ad92
Ben Kane's Win Tower <bbk1524@gmail.com>
2016-01-01 23:34:28 -0600

    tmpSource for loading xml

--- src/tmpSource.cpp
+++ src/tmpSource.cpp
@@ -4,6 +4,56 @@
 #include "tinyxml2-master/tinyxml2.h"
 #include "Definitions.h"
 
+void print_elements(tinyxml2::XMLElement *root_node)
+{
+    //if (!root_node) { return; }
+    namespace xml = tinyxml2;
+    std::cout << root_node->Attribute("name") << std::endl;
+
+    xml::XMLElement *info_node = nullptr;
+    int x1, y1, x2, y2;
+    int off_x1, off_y1, off_x2, off_y2;
+    float perc_x1, perc_x2, perc_y1, perc_y2;
+
+    info_node = root_node->FirstChildElement("position");
+    if (info_node)
+    {
+#define FILL_INT_POSITIONS(name) info_node->QueryIntAttribute(#name,&name)
+        info_node->QueryIntAttribute("x1", &x1);
+        FILL_INT_POSITIONS(y1);
+        FILL_INT_POSITIONS(x2);
+        FILL_INT_POSITIONS(y2);
+        std::cout << x1<< y1<< x2<< y2 << std::endl;;
+    }
+    info_node = root_node->FirstChildElement("offset");
+    if (info_node)
+    {
+        FILL_INT_POSITIONS(off_x1);
+        FILL_INT_POSITIONS(off_y1);
+        FILL_INT_POSITIONS(off_x2);
+        FILL_INT_POSITIONS(off_y2);
+        std::cout << off_x1<< off_y1<< off_x2<< off_y2 << std::endl;
+    }
+#undef FILL_INT_POSITIONS
+    info_node = root_node->FirstChildElement("percentage");
+    if (info_node)
+    {
+#define FILL_FLOAT_POSITIONS(name) info_node->QueryFloatAttribute(#name, &name)
+        FILL_FLOAT_POSITIONS(perc_x1);
+        FILL_FLOAT_POSITIONS(perc_y1);
+        FILL_FLOAT_POSITIONS(perc_x2);
+        FILL_FLOAT_POSITIONS(perc_y2);
+        std::cout << perc_x1<< perc_x2<< perc_y1<< perc_y2 << std::endl;
+
+#undef FILL_FLOAT_POSITIONS
+    }
+    for (xml::XMLElement *node = root_node->FirstChildElement("box"); node; node = node->NextSiblingElement("box"))
+    {
+        print_elements(node);
+    }
+}
+
+#undef main
 int main()
 {
     namespace xml = tinyxml2;
@@ -14,5 +64,8 @@ int main()
     {
         std::cout << "couldn't load" << std::endl;
     }
+    //xml::XMLElement* element = doc.FirstChildElement("box");
+    //std::cout << element->Attribute("name") << std::endl;
+    print_elements(doc.FirstChildElement("box"));
 }
 
