18b1fe31d8cfc777e4c97ecc5338949ff3ad05cd
ben-PC <bbk1524@gmail.com>
2016-09-24 23:37:30 -0500

    Start component redo. About to add dynamic object creation

--- src/main.cpp
+++ src/main.cpp
@@ -6,42 +6,77 @@
 #include <memory>
 
 
-struct Entity
+struct Pos
 {
-    SDL_Rect position;
-    Entity()
+    float x;
+    float y;
+    Pos(float x, float y) : x(x), y(y)
+    {}
+};
+
+struct Vel
+{
+    float x_pos;
+    float y_pos;
+    float x_neg;
+    float y_neg;
+    Vel(float x, float y)
+        : x_pos(x), x_neg(x), y_pos(y), y_neg(y)
+    {}
+};
+
+struct Shape
+{
+    SDL_Rect rect;
+    Shape(int x, int y, int w, int h)
     {
-        position.x = 100;
-        position.y = 100;
-        position.w = 50;
-        position.h = 50;
+        rect.x = x;
+        rect.y = y;
+        rect.w = w;
+        rect.h = h;
+    }
+    void adjust_to_pos(const Pos & pos)
+    {
+        rect.x = pos.x - rect.w / 2;
+        rect.y = pos.y - rect.h / 2; // Is it subtraction or addition?
     }
 };
 
-void update_renderer(SDL_Renderer *renderer, SDL_Rect *position)
+struct Entity
 {
+    Pos pos;
+    Vel vel;
+    Shape shape;
+
+    Entity(Pos && pos, Vel && vel, Shape && shape)
+        : pos(pos), vel(vel), shape(shape)
+    {}
+};
+
+void update_renderer(SDL_Renderer *renderer, Pos & pos, Shape & shape)
+{
+    shape.adjust_to_pos(pos);
     SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
-    SDL_RenderFillRect( renderer, position );
+    SDL_RenderFillRect( renderer, &(shape.rect) );
 }
 
-void update_position(Input_System& input_system, SDL_Rect *position)
+void update_position(Input_System & input_system, Pos & pos, Vel & vel)
 {
-    constexpr int speed = 20;
     if( input_system.get_event(game_event::UP) )
     {
-        position->y -= speed;
+        pos.y -= vel.y_neg;
     }
     if( input_system.get_event(game_event::DOWN) )
     {
-        position->y += speed;
+        pos.y += vel.y_pos;
     }
     if( input_system.get_event(game_event::LEFT) )
     {
-        position->x -= speed;
+        pos.x -= vel.x_neg;
     }
     if( input_system.get_event(game_event::RIGHT) )
     {
-        position->x += speed;
+        pos.x += vel.x_pos;
     }
 }
 
@@ -70,7 +105,8 @@ int main (int argc, char** argv)
     input_sytem.init();
     input_sytem.update();
 
-    Entity entity;
+    Entity entity(Pos(G_WINDOW_WIDTH/2, G_WINDOW_HEIGHT/2),
+        Vel(20, 20), Shape(0, 0, 30, 30));
 
     Timer timer;
 
@@ -84,8 +120,8 @@ int main (int argc, char** argv)
         // Clear winow
         SDL_RenderClear( renderer );
 
-        update_position(input_sytem, &entity.position);
-        update_renderer(renderer, &entity.position);
+        update_position(input_sytem, entity.pos, entity.vel);
+        update_renderer(renderer, entity.pos, entity.shape);
 
         // Render the rect to the screen
         SDL_RenderPresent(renderer);
